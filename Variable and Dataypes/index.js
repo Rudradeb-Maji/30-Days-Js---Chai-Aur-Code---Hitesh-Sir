971bbc79e037857956998de3541bb632899e7dc1
Rudradeb-Maji <Rudradeb-Maji>
2024-07-13 08:07:28 +0530

    #

--- Variable and Dataypes/index.js
+++ Variable and Dataypes/index.js
@@ -53,13 +53,3 @@ console.log("value = "+target + " " +"type = "+ typeof target);
 let array =["Day one - Done"];
 console.log("value = "+array + " " +"type = "+ typeof array );
 
-/* Feature Request: */
-// 2
-let commentCount = 5;
-commentCount=8; // No error ,because const type variable can be updated or modified
-console.log(commentCount);
-console.log(typeof commentCount);
-const editComment = false;
-editComment = true; // throws an error , because const type variable can't be updated or modified
-console.log(editComment); 
-console.log(typeof editComment);
